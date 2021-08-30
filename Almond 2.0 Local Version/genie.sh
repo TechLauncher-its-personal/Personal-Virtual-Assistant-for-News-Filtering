cad4030be816abf52e3e1a19d186e4c4e833c7f3
Anggriosutopo <u7162593@anu.edu.au>
2021-08-31 01:06:48 +0700

    Add file containing the commands used

---
+++ Almond 2.0 Local Version/genie.sh
@@ -0,0 +1,38 @@
+# Various commands used in the local Almond Version
+
+# Create a new project called its-personal-devices
+# Developer key obtained from the settings page of a developer account
+# at almond.stanford.edu
+genie init-project --developer-key $Developer-Key its-personal-devices
+
+# Move to the new folder that stores the devices
+cd its-personal-devices
+
+# Create a new device called org.itspersonal.newsfilter
+genie init-device org.itspersonal.newsfilter
+
+# At this stage, copy the files from the Almond News Filter Device folder
+# to the org.itspersonal.newsfilter folder. Scenarios.txt must be put
+# in the org.itspersonal.newsfilter/eval folder.
+
+# Test the device using the scenario test
+node ./test/scenarios org.itspersonal.newsfilter
+
+# Pack the files of the device into a zip file for uploading
+npx make build/org.itspersonal.newsfilter.zip
+
+# Upload the device to the main Almond website for testing
+# Access token obtained from the settings page of the account at
+# almond.stanford.edu
+genie upload-device \
+  --access-token $Access-Token \
+  --zipfile build/org.itspersonal.newsfilter.zip \
+  --icon org.itspersonal.newsfilter/icon.png \
+  --manifest org.itspersonal.newsfilter/manifest.tt \
+  --dataset org.itspersonal.newsfilter/dataset.tt
+
+# Run the local almond
+genie assistant --thingpedia-dir its-personal-devices
+
+# Example command to call the news filter device
+\t @org.itspersonal.newsfilter.news_article(topic = enum sports);
